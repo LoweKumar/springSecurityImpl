cb2525339ec14bf6b5da430a77afcf38ad22f827
Lowe Kumar <lowekr00@gmail.com>
2026-03-07 18:16:04 +0530

    Section4 code implementation - custom EazyBankUserDetailsService implemented

--- src/main/java/com/springSecurityImpl/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/springSecurityImpl/springsecurity/config/ProjectSecurityConfig.java
@@ -4,14 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,9 +17,10 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
 //        http.authorizeHttpRequests((request)->request.anyRequest().authenticated());
-        http.authorizeHttpRequests((requests) -> requests
+        http.csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                .requestMatchers("/notices", "/contact", "/error").permitAll());
+                .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
@@ -31,17 +28,17 @@ public class ProjectSecurityConfig {
 
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(DataSource datasource) {
-//        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
-//        UserDetails admin = User.withUsername("admin")
-//                .password("{bcrypt}$2a$12$88.f6upbBvy0okEa7OfHFuorV29qeK.sVbB9VQ6J6dWM1bW6Qef8m")
-//                .authorities("admin").build();
-//        return new InMemoryUserDetailsManager(user, admin);
-
-        //using JdbcUserDetailsManager
-        return new JdbcUserDetailsManager(datasource);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource datasource) {
+////        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
+////        UserDetails admin = User.withUsername("admin")
+////                .password("{bcrypt}$2a$12$88.f6upbBvy0okEa7OfHFuorV29qeK.sVbB9VQ6J6dWM1bW6Qef8m")
+////                .authorities("admin").build();
+////        return new InMemoryUserDetailsManager(user, admin);
+//
+//        //using JdbcUserDetailsManager
+//        return new JdbcUserDetailsManager(datasource);
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
---
+++ src/main/java/com/springSecurityImpl/springsecurity/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.springSecurityImpl.springsecurity.repository;
+
+import com.springSecurityImpl.springsecurity.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+}
